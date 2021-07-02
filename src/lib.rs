c0468192fbff8b1487852b1fd226debe0558d018
C Ding <cding@cs.rochester.edu>
2021-07-02 10:10:15 -0400

    test filter

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,14 @@ mod tests{
     #[test]
     fn test_scan() {
         let a = [1, 2, 3];
-        let mut iter2 = a.iter().scan(0, |mut acc, e| {*acc += e; Some(*acc)});
-        let prefix_sum : Vec<i32> = iter2.collect();
+        let prefix_sum : Vec<i32> = a.iter().scan(0, |acc, e| {*acc += e; Some(*acc)}).collect();
         assert_eq!(prefix_sum, [1,3,6]);
+    }
+
+    #[test]
+    fn test_filter() {
+        let a = [1, 2, 3];
+        let greaterthan1 : Vec<i32> = a.iter().filter(|&&x| x > 1).map(|x| *x).collect();
+        assert_eq!(greaterthan1, [2,3]);
     }
 }
